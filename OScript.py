f8f15b29f9dfb623bb6dd28438e08906fa86c927
paul-dot-dll <47700340+paul-dot-dll@users.noreply.github.com>
2020-12-26 00:10:30 +0000

    Update OScript.py
    Made program run until it is closed, not when quitting after running a command.

--- OScript.py
+++ OScript.py
@@ -1,9 +1,11 @@
-command = input()
-if command.startswith('echo'):
-    # print(command) (used to check if string formatting was correct
-    cmd = command.partition('echo ')[2]
-    runcmd = "print ('" + cmd + "')"
-    #print(runcmd) (was also used to check if string was done correctly)
-    exec(runcmd)
-if command.startswith('help') or command == "help" or command == "/?":
-    print("Commands include:\n *echo (arg) \\\returns any string inputted\n *help \\\lists all current commands")
+while True:
+    command = input()
+    if command.startswith('echo'):
+        # print(command) (used to check if string formatting was correct
+        cmd = command.partition('echo ')[2]
+        runcmd = "print ('" + cmd + "')"
+        #print(runcmd) (was also used to check if string was done correctly)
+        exec(runcmd)
+    if command.startswith('help') or command == "help" or command == "/?":
+        print("Commands include:\n *echo (arg) \\\returns any string inputted\n *help \\\lists all current commands")
+
