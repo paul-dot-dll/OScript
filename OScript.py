6f8948d768bb5a298698a8d32891c18cf9d45a32
paul-dot-dll <47700340+paul-dot-dll@users.noreply.github.com>
2020-12-26 02:46:34 +0000

    Update OScript.py
    Apparently I didn't delete the unused part, just deleting for sure this time.

--- OScript.py
+++ OScript.py
@@ -24,15 +24,3 @@ def GUIRUN(command): #the reason this one doesn't have a forever loop is because
 if __name__ == "__main__": #this splits up the code into two parts, if it's running from the UI program (as a module), then it uses GUIRUN, if you run this file, it uses the original one.
     main()
 
-'''
-while True:
-    command = input()
-    if command.startswith('echo'):
-        # print(command) (used to check if string formatting was correct
-        cmd = command.partition('echo ')[2]
-        runcmd = "print ('" + cmd + "')"
-        #print(runcmd) (was also used to check if string was done correctly)
-        exec(runcmd)
-    if command.startswith('help') or command == "help" or command == "/?":
-        print("Commands include:\n *echo (arg) \\\returns any string inputted\n *help \\\lists all current commands")
-'''
